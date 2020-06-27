29250324256b3f05dd3362b6d0fc2cb89e726965
ZH-TX <841648947@qq.com>
2020-06-27 22:41:23 +0800

    '链表的各种形式'

--- js数据结构与算法/04-1双向链表.js
+++ js数据结构与算法/04-1双向链表.js
@@ -1,112 +1,16 @@
+/**
+ * 1.环形链表, 单向环形, 双向环形
+ * 
+ */
 
 
- /**
-  * 约瑟夫问题: 单向环形链表的应用
-  * 环形: 链表的首尾需要连接起来
-  */
- class NodeList{
-    constructor(rank){
-        this.rank=rank
-        this.next=null
-    }
-    // getRank(){
-    //     return this.rank
-    // }
-    // setRank(rank){
-    //     return this.rank=rank
-    // }
-    // getNext(){
-    //     return this.next //实现循环
-    // }
-    // setNext(next){
-    //     return this.next=next
-    // }
-
-    logNode(){
-        console.log(`rank:${this.rank}`);   
-    }
-}
-class circleNode{
-    constructor(){
-        this.first=new NodeList(1)
-    }
-
-    handleNode(num){
-        let curLast=this.first    //辅助指针
-        
-        for (let i = 1; i < num+1; i++) {
-            if(i==1){
-                this.first.next=this.first
-            }else{
-                let node=new NodeList(i)
-                curLast.next=node
-                node.next=this.first
-                curLast=node
-            }
-           
-        }
-       
-    }
-
-    insertNode(rank){
-        let temp=this.head
-        while(true){
-            if(temp.next==null){//需要将最后的节点指向第一个节点
-                temp.next=this.head
-                break;
-            }
-            
-
-            if(rank<temp.next.rank){
-               break;
-            }
-            temp=temp.next
-        }
-        let temp2=temp.next
-        let newTemp=new NodeList(rank)
-        temp.next=newTemp
-        newTemp.next=temp2
-    }  
-
-    delNode(rank){
-        let temp=this.head
-        while(true){
-            if(temp.next==null)break;
-            if(rank==temp.next.rank)break;
-            temp=temp.next         
-        }
-        let temp2=temp.next
-        temp.next=temp2.next
-    }
-    
-    showNode(){
-        let temp=this.first
-        // if(this.isEmpty())return;
-        while(true){
-            temp.logNode()
-            if(temp.next==this.first)break;
-            temp=temp.next   
-        }
-    }
-}
-
-let a=new circleNode()
-a.handleNode(5)
-// a.delNode(3)
-
-a.showNode()
-console.log(a);
-
-
-
-
-
-  //nodelist类
+//双向链表的实现
 class NodeList{
     constructor(rank,name,desc){
         this.rank=rank
         this.name=name
         this.desc=desc
+        this.prev
         this.next
     }
     showNode(){
@@ -126,12 +30,11 @@ class singleNode{
         return this.head.next==null;
     }
 
-
     addNode(rank,name,desc){ //这个Node需要吧实现Nodelist实例当做参数{rank,name,desc}
        let temp=this.head
        //遍历链表找到最后
        //非连续结构的遍历, temp相当于index 指针
-       let rankNo;
+       
        while(true){
             if(temp.next==null){
                 break;
@@ -140,7 +43,10 @@ class singleNode{
             temp=temp.next 
        }
        //循环结束添加的新节点指向最后  添加
-       temp.next=new NodeList(rank,name,desc)   
+       let newTemp=new NodeList(rank,name,desc)
+       temp.next=newTemp
+       newTemp.prev=temp
+
     }
            //若要实现按照编号顺序添加,
     /**
@@ -155,13 +61,7 @@ class singleNode{
         // let rankNo;
         while(true){
             if(temp.next==null)break;
-            // if(temp.rank<rank<temp.next.rank) {
-            //     let temp2=temp.next
-            //     let newTemp=new NodeList(rank,name,desc)
-            //     temp.next=newTemp
-            //     newTemp.next=temp2
-            // }
-            // temp=temp.next 
+            
             if(rank<temp.next.rank){
                break;
             }
@@ -170,8 +70,11 @@ class singleNode{
         }
         let temp2=temp.next
         let newTemp=new NodeList(rank,name,desc)
+
         temp.next=newTemp
+        newTemp.prev=temp
         newTemp.next=temp2
+        temp2.prev=newTemp
         
     }  
 
@@ -190,6 +93,8 @@ class singleNode{
         }
         let temp2=temp.next
         temp.next=temp2.next
+        temp2.next.prev=temp2.prev
+        // temp2.next.prev=temp
         // newTemp.next=temp2
     }
     
@@ -216,9 +121,7 @@ let a=new singleNode()
 a.addNode(1,'孙悟空','跑得快')
 a.addNode(3,'沙和尚','做的多')
 a.insertNode(2,'猪八戒','吃的多')
-a.delNode(3)
+a.delNode(2)
 
 a.showNode()
 console.log(a);
-
-
